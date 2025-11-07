59dee147d6f70cbcdfb82489f868a71c1cce6503
AmanPatre <amanpatre25@gmail.com>
2025-11-07 15:07:09 +0530

    Doctor listing api

--- src/doctors/doctors.service.ts
+++ src/doctors/doctors.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDoctorDto } from './dto/create-doctor.dto';
 import { UpdateDoctorDto } from './dto/update-doctor.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Doctor } from './entities/doctor.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DoctorsService {
+  constructor(
+    // 1. Inject the Doctor Repository
+    @InjectRepository(Doctor)
+    private doctorRepository: Repository<Doctor>,
+  ) {}
+
   create(createDoctorDto: CreateDoctorDto) {
     return 'This action adds a new doctor';
   }
 
   findAll() {
-    return `This action returns all doctors`;
+    return this.doctorRepository.find({
+      relations: ['user'],
+    });
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} doctor`;
   }
 
-  update(id: number, updateDoctorDto: UpdateDoctorDto) {
+  update(id: string, updateDoctorDto: UpdateDoctorDto) {
     return `This action updates a #${id} doctor`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} doctor`;
   }
 }
